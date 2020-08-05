848872eb5f6589a5e0dbc5dbec8b4ee5c14fae2f
firas <firastayeb1@gmail.com>
2020-08-05 21:35:30 +0200

    Update app.js

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ class Sidebar extends Component {
   constructor(props) {
     super(props);
     this.app = this.props.sdk.app;
+    console.log('this.props.sdk.parameters.installation: ', this.props.sdk.parameters.installation)
     this.appConfigParameters = this.props.sdk.parameters.installation;
   }
 
